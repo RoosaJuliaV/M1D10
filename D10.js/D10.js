1314222ca86e9fef1d11f5dd55622fb3f0efce0c
RoosaJuliaV <roosavuotso@hotmail.com>
2021-07-02 13:50:34 +0200

    Exercise 27

--- D10.js/D10.js
+++ D10.js/D10.js
@@ -14,6 +14,12 @@ console.log(colours)
 let randomNumber = Array.from({length: 100}, () => Math.floor(Math.random() * 200))
 console.log(randomNumber)
 
+Math.min(...randomNumbers)
+console.log(Math.min(...randomNumbers))
+
+Math.max(...randomNumbers)
+console.log(Math.max(...randomNumbers))
+
         /* 21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
         22) Create an object with properties such name, surname, email
         23) Delete Email from the previously created object
