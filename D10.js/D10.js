6a3940306749fa19bfb818f70eb5a46e730bf8d4
RoosaJuliaV <roosavuotso@hotmail.com>
2021-07-02 15:57:29 +0200

    Exercise 28

--- D10.js/D10.js
+++ D10.js/D10.js
@@ -25,6 +25,16 @@ console.log(Math.min(...randomNumbers))
 Math.max(...randomNumbers)
 console.log(Math.max(...randomNumbers))
 
+let emptyArray = []
+for (let i = 0; i < 10; i++) {
+    let anotherEmptyArray = []
+    for (let n = 0; n < 10; n++) {
+        anotherEmptyArray.push(Math.random() * 10)
+    }
+    emptyArray.push(Math.random() * 10)
+}
+console.log(emptyArray)
+
 const animalArray = ["dog", "cat", "bear"]
 const rodentArray = ["hamster", "guinea pig"]
     
@@ -36,6 +46,14 @@ const rodentArray = ["hamster", "guinea pig"]
     }
   }
 console.log(returnLongest(animalArray, rodentArray))
+
+let arrayOfNumbers = [6, 4, 3]
+let sum = 0
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+}
+console.log(sum)
 
         /* 21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
         22) Create an object with properties such name, surname, email
